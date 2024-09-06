b96fcccb693a809bacd9adc43b1f564c52299210
OsvaldoCorneli <overcorneli@gmail.com>
2024-09-06 19:50:23 -0300

    borre console.log

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -8,14 +8,13 @@ import data from "../../../public/data/colegas.json"
 function PopUp() {
 
     const [ishidden, setIsHidden] = useState(false)
-    const [contador , setContador] = useState(10)
-    const [btnFlag , setBtnFlag] = useState(false)
+    const [contador, setContador] = useState(10)
+    const [btnFlag, setBtnFlag] = useState(false)
 
     function handlerBtnCerrar() {
         setIsHidden(false)
         setBtnFlag(false)
         handlerPublicity()
-        console.log("funcion", ishidden, btnFlag)
     }
 
     function handlerBtnNoMolestar() {
@@ -32,85 +31,81 @@ function PopUp() {
     }
 
     useEffect(() => {
-            console.log("contador , ishidden , btnflag", "\n" ,contador , ishidden , btnFlag)
-        if(contador > 0 && ishidden){
+        if (contador > 0 && ishidden) {
 
-            setTimeout(()=>{
-                // console.log("contador", contador)
-                // console.log("desdeSettime flag", btnFlag)
+            setTimeout(() => {
                 let newContador = contador - 1;
                 setContador(newContador)
-            },1000)
+            }, 1000)
 
-        }else if(ishidden){
-            console.log("ingresa al else")
+        } else if (ishidden) {
+
             setBtnFlag(true)
         }
 
 
-        console.log("ishidden antes if", ishidden)
-        if( !ishidden){
-            console.log("ishidden desde if", ishidden)
+
+        if (!ishidden) {
             handlerPublicity()
         }
-    
+
 
     }, [contador, ishidden])
 
-    
+
 
 
 
 
     return (
         <>
-        { ishidden 
-        
-        ? <span className={style.contenedor_popUp}>
-            <div className={style.popUps}>
-                <div className={style.popups_close}>
-                    <a className={style.btn_popup} onClick={handlerBtnCerrar} ><FontAwesomeIcon icon={faRectangleXmark} /></a>
-                </div>
-                <div className={style.popups_message}>
-                    <h2>Gracias por visitar mi portfolio</h2>
-                    <p>Aprecio mucho que te hayas tomado el tiempo para revisar mi trabajo.
-                        Si mi perfil no encaja con lo que estás buscando, te invito a explorar
-                        los portfolios de algunos colegas talentosos. ¡Espero que encuentres
-                        lo que necesitas!</p>
-                </div>
+            {ishidden
 
-                {
-                    data.length > 0
-                        ? data.map((datos) => (
+                ? <span className={style.contenedor_popUp}>
+                    <div className={style.popUps}>
+                        <div className={style.popups_close}>
+                            <a className={style.btn_popup} onClick={handlerBtnCerrar} ><FontAwesomeIcon icon={faRectangleXmark} /></a>
+                        </div>
+                        <div className={style.popups_message}>
+                            <h2>Gracias por visitar mi portfolio</h2>
+                            <p>Aprecio mucho que te hayas tomado el tiempo para revisar mi trabajo.
+                                Si mi perfil no encaja con lo que estás buscando, te invito a explorar
+                                los portfolios de algunos colegas talentosos. ¡Espero que encuentres
+                                lo que necesitas!</p>
+                        </div>
 
-                            <CardPopUp
-                                key={datos.id}
-                                name={datos.name}
-                                tittle={datos.tittle}
-                                linkedin={datos.linkedin}
-                                github={datos.github}
-                                portfolio={datos.portfolio}
+                        {
+                            data.length > 0
+                                ? data.map((datos) => (
 
-                            />
+                                    <CardPopUp
+                                        key={datos.id}
+                                        name={datos.name}
+                                        tittle={datos.tittle}
+                                        linkedin={datos.linkedin}
+                                        github={datos.github}
+                                        portfolio={datos.portfolio}
 
-
-                        ))
-                        : ""
-                }
+                                    />
 
 
+                                ))
+                                : ""
+                        }
 
-                <div className={style.popups_btn}>
-                    {btnFlag
-                    ?<a className={style.btn_popup} onClick={handlerBtnNoMolestar}>No volver a mostrar</a>
-                    :<span className={style.btn_popup_span}>No volver a mostrar {contador}</span>
-                    }
-                </div>
-            </div>
-        </span>
-        : ""
-    } 
-    </>)
+
+
+                        <div className={style.popups_btn}>
+                            {btnFlag
+                                ? <a className={style.btn_popup} onClick={handlerBtnNoMolestar}>No volver a mostrar</a>
+                                : <span className={style.btn_popup_span}>No volver a mostrar {contador}</span>
+                            }
+                        </div>
+                    </div>
+                </span>
+                : ""
+            }
+        </>)
 }
 
 export default PopUp;
